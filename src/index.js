a74984ba3d3ec29eb5e1a54130e5aed5d3bfff48
Patryk Podolski <patryk.podolski@nokia.com>
2018-04-30 13:39:07 +0200

    folders rearanged

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+
+class Sth extends React.Component {
+  render() {
+    return(<h1>sadsad</h1>);
+  }
+}
+
+export default Sth;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
